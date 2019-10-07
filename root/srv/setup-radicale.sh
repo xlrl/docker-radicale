930bb31d9e5870d7cbc07ce63a6f512662f7b5f8
Alexander Mueller <xelarellum@web.de>
2019-10-07 02:08:35 +0200

    Setup git on start as well

--- root/srv/setup-radicale.sh
+++ root/srv/setup-radicale.sh
@@ -3,4 +3,7 @@ set -e
 
 echo "Setup: fix rights for radicale..."
 chown radicale.radicale -R /var/radicale
+echo "Setup: fix git settings..."
+git config --global user.name radicale
+git config --global user.email radicale@localhost
 echo "Setup: Done"
